2ad7405dc67c72e92e9d24d451f218d0cb6ee77e
kevin-vargas <kevinn.vargass@gmail.com>
2020-10-20 04:36:09 -0300

    async await

--- src/script.py
+++ src/script.py
@@ -2,15 +2,20 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import telegram_url
+import asyncio
 
 TELEGRAM_URL = telegram_url.api_uri
 
 URL = 'https://compragamer.com/index.php?criterio=3070&x=0&y=0&seccion=3&nro_max=50'
 
-has_product_flag = True
+def getUrl(item):
+    return 'https://compragamer.com/index.php?criterio='+item+'&x=0&y=0&seccion=3&nro_max=50'
 
-while True:
-    page = requests.get(URL)
+async def check(url,value):
+    has_product_flag = True
+    loop = asyncio.get_event_loop()
+    future = loop.run_in_executor(None, requests.get, url)
+    page = await future
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # este string dice la cantidad de productos de acuerdo a los query params de arriba
@@ -23,11 +28,20 @@ while True:
     if "1 - 0" not in has_products_string:
         # queremos una sola notif de si llego el producto
         if has_product_flag and not has_stock:
-            print("Hay productos de la 3070 pero no hay stock")
-            requests.get(TELEGRAM_URL + 'Atento maestro, parece que la 3070 ha arribado')
+            requests.get(TELEGRAM_URL + 'Atento maestro, parece que la '+value+' ha arribado')
+            print('Hay productos de la'+ value +' pero no hay stock')
             has_product_flag = False
         elif has_stock:
-            requests.get(TELEGRAM_URL + 'LLEGO LA 3070 PAPA HORA DE COMPRAR')
-            print("LLEGO LA 3070 SUPREMA")
-    # GET cada 120s
+            requests.get(TELEGRAM_URL + 'LLEGO LA '+value+' PAPA HORA DE COMPRAR')
+            print('LLEGO LA '+value+' SUPREMA')
+
+
+products = ["580","3070","3080"]
+
+async def main():
+    values = await asyncio.gather(*[check(getUrl(i),i) for i in products])
+
+while True:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
     time.sleep(120)
